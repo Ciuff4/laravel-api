c8bd8a38031da9a9ff496c433c9c25afb5564e3e
Ciuff4 <gasperini_cristina@libero.it>
2022-07-15 03:49:54 +0200

    add:divisione front e admin scss/js+chiamata axios

---
+++ resources/js/AppComponent.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="container">
+    <h1>App vue component</h1>
+<h3>I titoli dei post</h3>
+<ul>
+    <li
+    v-for="post in posts"
+    :key="post.id">
+    <p>{{post.title}}</p>
+    </li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+name:'App',
+data(){
+return{
+    apiUrl:'http://127.0.0.1:8000/api/posts'
+}
+},
+mounted(){
+this.getApiUrl()
+},
+methods:{
+    getApiUrl(){
+        axios.get(this.apiUrl)
+        .then(r=>{
+            console.log(r.data);
+        })
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
